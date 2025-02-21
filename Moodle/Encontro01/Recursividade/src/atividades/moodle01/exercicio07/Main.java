94d4a7a450c324e1854006ee661caed865d7671d
marielle-vaz <mariellerodrigues62@gmail.com>
2025-02-20 23:46:21 -0300

    update

--- Moodle/Encontro01/Recursividade/src/atividades/moodle01/exercicio07/Main.java
+++ Moodle/Encontro01/Recursividade/src/atividades/moodle01/exercicio07/Main.java
@@ -2,10 +2,12 @@ package atividades.moodle01.exercicio07;
 
 import java.io.File;
 
+import javax.swing.JOptionPane;
+
 public class Main {
     public static void main(String[] args) {
+       /* 
         File folder = new File("C:\\Users\\marie\\Desktop\\Instituto Federal Goiano - Campus Trindade"); // current directory
-
         File[] listOfFiles = folder.listFiles(); // list all files in the current directory
 
         for (File file : listOfFiles) {
@@ -14,6 +16,42 @@ public class Main {
             System.out.println("Is directory: " + file.isDirectory()); // true if the file is a directory
             System.out.println("Is file: " + file.isFile()); // true if the file is a regular file
             System.out.println("");
+        }*/ 
+
+        String pasta = JOptionPane.showInputDialog("Informe a raíz da sua estrutura de pastas (Exemplo: C:\\Users\\marie\\Desktop)"); // Diretório atual
+        String nomeArq = JOptionPane.showInputDialog("Informe o nome e a extensão do arquivo (Exemplo: AtividadeEstrutura.pdf)");
+        
+        boolean encontrado = buscarArquivo(pasta, nomeArq);
+        System.out.println("Arquivo encontrado: " + encontrado);
+
+        if(encontrado){
+            JOptionPane.showMessageDialog(null, "Encontrei " + qtde + " arquivo(s)", "ENCONTRADO", JOptionPane.INFORMATION_MESSAGE);
         }
     }
+
+    static int qtde = 0;
+
+    private static boolean buscarArquivo(String caminhoOrigem, String nomeArquivo) {
+        File arq = new File(caminhoOrigem);
+        
+        if (!arq.exists() || !arq.isDirectory()) {
+            JOptionPane.showMessageDialog(null, "Arquivo ou pasta informada inexistente!", "ERRO", JOptionPane.ERROR_MESSAGE);
+            return false;
+        }
+        
+        File[] arquivos = arq.listFiles();
+        if (arquivos != null) {
+            for (File arquivo : arquivos) {
+                if (arquivo.isFile() && arquivo.getName().equals(nomeArquivo)) {
+                    qtde++;
+                    return true;
+                } else if (arquivo.isDirectory() && buscarArquivo(arquivo.getAbsolutePath(), nomeArquivo)) {
+                    qtde++;
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+    
 }
